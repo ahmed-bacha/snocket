1aaab974e5135c8ce03e56ed046c8b51956db7ea
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-12-05 03:51:49 +0100

    Change server loading page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http'),
     fs = require('fs'),
     // NEVER use a Sync function except at start-up!
-    index = fs.readFileSync(__dirname + '/mobile.html');
+    index = fs.readFileSync(__dirname + '/index.html');
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
