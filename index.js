85af7c24c01d080682fcabad3b27ee0a7076968a
Mehdi Lahmam <mehdi@craftsmen.io>
2014-12-05 01:14:20 +0100

    Add a basic node server with socket.io example

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/index.html');
+
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+});
+
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+// Emit welcome message on connection
+io.sockets.on('connection', function(socket) {
+  socket.emit('welcome', { message: 'Welcome!' });
+
+  socket.on('left', function(){
+    socket.emit('snake_move', {direction: 'left'})
+  });
+  //socket.on('right', console.log);
+});
+
+app.listen(3000);
